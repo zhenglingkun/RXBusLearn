b6a2e29cbb26c11a7232c8c3a7e0fa0a6bb385e9
hbg <hbg@servyou.com.cn>
2017-05-08 19:18:12 +0800

    主界面修改

--- app/src/main/java/zlk/com/rxbuslearn/MainActivity.java
+++ app/src/main/java/zlk/com/rxbuslearn/MainActivity.java
@@ -1,32 +1,28 @@
 package zlk.com.rxbuslearn;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.Button;
+import android.view.View;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
 import rx.Observable;
-import rx.Subscription;
 import rx.functions.Action1;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = MainActivity.class.getSimpleName();
 
-    private Subscription mSubscription;
-
-    @BindView(R.id.intent_bt)
-    Button mIntentBt;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
+
+        findViewById(R.id.intent_bt).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
         Observable.just("123", "300")
                 .last()
@@ -38,40 +34,4 @@ public class MainActivity extends AppCompatActivity {
                 });
     }
 
-    @OnClick(R.id.intent_bt)
-    public void onClick() {
-        RxBus.getDefault().send(new TestEvent("Java"));
-        startActivity(new Intent(MainActivity.this, RxBusActivity.class));
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-//        mSubscription = RxBus.getDefault()
-//                .toObservable().subscribe(new Action1<Object>() {
-//                    @Override
-//                    public void call(Object o) {
-//                        if (o instanceof TestEvent) {
-//                            Log.i(TAG, "call: name = " + ((TestEvent) o).getName());
-//                        } else {
-//                            Log.i(TAG, "call: o = " + o.toString());
-//                        }
-//                    }
-//                }, new Action1<Throwable>() {
-//                    @Override
-//                    public void call(Throwable throwable) {
-//                        Log.e(TAG, "call: ", throwable);
-//                    }
-//                });
-
-        mSubscription = RxBus.getDefault().toObservable(Object.class).subscribe();
-    }
-
-    @Override
-    protected void onDestroy() {
-        if (mSubscription != null && !mSubscription.isUnsubscribed()) {
-            mSubscription.unsubscribe();
-        }
-        super.onDestroy();
-    }
 }
