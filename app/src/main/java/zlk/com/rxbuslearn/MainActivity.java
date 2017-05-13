5733a28cdc6c3805fc0c29c46956943e8720a6c3
hbg <hbg@servyou.com.cn>
2017-05-13 14:06:02 +0800

    获取系统通讯录

--- app/src/main/java/zlk/com/rxbuslearn/MainActivity.java
+++ app/src/main/java/zlk/com/rxbuslearn/MainActivity.java
@@ -1,7 +1,10 @@
 package zlk.com.rxbuslearn;
 
+import android.content.ContentResolver;
 import android.content.Context;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -14,12 +17,13 @@ import android.widget.TextView;
 import java.util.ArrayList;
 import java.util.List;
 
+import zlk.com.rxbuslearn.pinnedheaderlistview.ContactPinnedAdapter;
 import zlk.com.rxbuslearn.pinnedheaderlistview.CustomerBean;
 import zlk.com.rxbuslearn.pinnedheaderlistview.LetterAdapter;
 import zlk.com.rxbuslearn.pinnedheaderlistview.LetterBean;
 import zlk.com.rxbuslearn.pinnedheaderlistview.PinnedHeaderBean;
 import zlk.com.rxbuslearn.pinnedheaderlistview.PinnedHeaderListView;
-import zlk.com.rxbuslearn.pinnedheaderlistview.ContactPinnedAdapter;
+import zlk.com.rxbuslearn.util.FirstLetterUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -106,6 +110,69 @@ public class MainActivity extends AppCompatActivity {
                 listView.setSelection(section);
             }
         });
+    }
+
+    /**
+     * 获取通讯录联系人
+     * @param customerBeanList
+     */
+    private void initContactList(List<CustomerBean> customerBeanList) {
+        //生成ContentResolver对象
+        ContentResolver contentResolver = getContentResolver();
+        // 获得所有的联系人
+        // 在这里我们给query传递进去一个SORT_KEY_PRIMARY。
+        // 告诉ContentResolver获得的结果安装联系人名字的首字母有序排列。
+        Log.i("contact", "initData: ContactsContract.CommonDataKinds.Phone.CONTENT_URI = " + ContactsContract.CommonDataKinds.Phone.CONTENT_URI);
+        Log.i("contact", "initData: ContactsContract.CommonDataKinds.Phone.CONTENT_URI = " + ContactsContract.Contacts.CONTENT_URI);
+        Cursor cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null,
+                ContactsContract.Contacts.SORT_KEY_PRIMARY);
+        if (cursor == null) {
+            return;
+        }
+
+        // 循环遍历
+        if (cursor.moveToFirst()) {
+            CustomerBean customerBean;
+
+            int idColumn = cursor.getColumnIndex(ContactsContract.Contacts._ID);
+            int displayNameColumn = cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME);
+
+            do {
+                customerBean = new CustomerBean();
+
+                // 获得联系人的ID
+                String contactId = cursor.getString(idColumn);
+
+                // 获得联系人姓名
+                String displayName = cursor.getString(displayNameColumn);
+                customerBean.setContactName(displayName);
+                customerBean.setNamePinyin(FirstLetterUtil.getFirstLetter(displayName.trim()).toUpperCase());
+
+                List<String> phones = new ArrayList<>();
+                // 查看联系人有多少个号码，如果没有号码，返回0
+                int phoneCount = cursor.getInt(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER));
+                if (phoneCount > 0) {
+                    // 获得联系人的电话号码列表
+                    Cursor phoneCursor = contentResolver.query(
+                            ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                            null,
+                            ContactsContract.CommonDataKinds.Phone.CONTACT_ID
+                                    + "=" + contactId, null, null);
+                    if (phoneCursor == null) {
+                        return;
+                    }
+                    if (phoneCursor != null && phoneCursor.moveToFirst()) {
+                        do {
+                            //遍历所有的联系人下面所有的电话号码
+                            String phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                            phones.add(phoneNumber);
+                        } while (phoneCursor.moveToNext());
+                    }
+                }
+                customerBean.setPhones(phones);
+                customerBeanList.add(customerBean);
+            } while (cursor.moveToNext());
+        }
     }
 
 }
--- app/src/test/java/zlk/com/rxbuslearn/ExampleUnitTest.java
+++ app/src/test/java/zlk/com/rxbuslearn/ExampleUnitTest.java
@@ -2,7 +2,9 @@ package zlk.com.rxbuslearn;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import zlk.com.rxbuslearn.util.FirstLetterUtil;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
@@ -11,5 +13,14 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void firstLetter() {
+        assertEquals("a", FirstLetterUtil.getFirstLetter("啊"));
+        assertEquals(" a", FirstLetterUtil.getFirstLetter(" 啊"));
+        assertEquals("akk", FirstLetterUtil.getFirstLetter("akk"));
+        assertEquals("#kk", FirstLetterUtil.getFirstLetter("#kk"));
+        assertEquals("#aassa", FirstLetterUtil.getFirstLetter("#啊啊ss啊"));
     }
 }
