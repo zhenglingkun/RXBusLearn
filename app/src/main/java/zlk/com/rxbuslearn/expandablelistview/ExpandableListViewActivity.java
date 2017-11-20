de9dcb5a5e4f884656ae8568d5c24748978a5821
hbg <hbg@servyou.com.cn>
2017-11-20 14:14:08 +0800

    ExpandableListView 点击事件处理

--- app/src/main/java/zlk/com/rxbuslearn/expandablelistview/ExpandableListViewActivity.java
+++ app/src/main/java/zlk/com/rxbuslearn/expandablelistview/ExpandableListViewActivity.java
@@ -3,6 +3,7 @@ package zlk.com.rxbuslearn.expandablelistview;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ExpandableListView;
 
 import java.util.ArrayList;
@@ -39,6 +40,21 @@ public class ExpandableListViewActivity extends AppCompatActivity {
             data.add(first);
         }
         expandableListView.setAdapter(new ElvTestAdapter(data));
+        //        设置分组项的点击监听事件
+        expandableListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+            @Override
+            public boolean onGroupClick(ExpandableListView expandableListView, View view, int i, long l) {
+                // 请务必返回 false，否则分组不会展开
+                return false;
+            }
+        });//        设置子选项点击监听事件
+        expandableListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition,
+                                        int childPosition, long id) {
+                return true;
+            }
+        });
     }
 
 }
