3bd73e39dda9ca10dc85e4dce2c4e68e60faaafb
hbg <hbg@servyou.com.cn>
2018-09-14 14:47:57 +0800

    ADDED - 添加了漰溃日志收集代码；

--- app/src/main/java/zlk/com/rxbuslearn/base/BaseApplication.java
+++ app/src/main/java/zlk/com/rxbuslearn/base/BaseApplication.java
@@ -1,8 +1,14 @@
 package zlk.com.rxbuslearn.base;
 
 import android.app.Application;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
 
 import zlk.com.rxbuslearn.log.LogConfigManager;
+import zlk.com.rxbuslearn.util.FileUtils;
+import zlk.com.rxbuslearn.util.LogUtil;
 
 /**
  * @author zlk
@@ -11,9 +17,36 @@ import zlk.com.rxbuslearn.log.LogConfigManager;
 
 public class BaseApplication extends Application {
 
+    private static final String TAG = BaseApplication.class.getSimpleName();
+
     @Override
     public void onCreate() {
         super.onCreate();
         LogConfigManager.getInstance(this, "KClientLog").OpenLog();
+
+        configUncaughtExceptionHandler();
     }
+
+    /**
+     * 捕获异常
+     */
+    private void configUncaughtExceptionHandler() {
+        Thread.setDefaultUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+
+            @Override
+            public void uncaughtException(Thread thread, Throwable ex) {
+                LogUtil.e(TAG, "uncaughtException crash");
+                Toast.makeText(BaseApplication.this, "crash", Toast.LENGTH_LONG).show();
+                try {
+                    ex.printStackTrace(new PrintStream(FileUtils.createErrorFile()));
+                } catch (FileNotFoundException e) {
+                    LogUtil.e(TAG, "创建异常文件失败");
+                    e.printStackTrace();
+                }
+
+            }
+
+        });
+    }
+
 }
